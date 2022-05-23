7a21172736b4212dd21d27ef952fa85de91a2a0f
Oachi <ongsa.srp.mkm@gmail.com>
2022-05-23 12:52:38 +0700

    fixed wrong subjectId and auto save to file

--- linkJSonTranformer.js
+++ linkJSonTranformer.js
@@ -1,15 +1,24 @@
-link = require('./link.json')
+const { writeFile } = require('fs')
+const link = require('./link.json')
 
 const newObj = {}
 
-console.log(link)
+// console.log(link)
 
 for (let item of link) {
     if('duplicate' in item) newObj[`${item.id}${item.duplicate}`] =  'https://' + item.link
     else newObj[item.id] = 'https://' + item.link
 }
 
+console.log(newObj)
 
-console.log(
-    JSON.stringify(newObj)
-)
+const objString = JSON.stringify(newObj)
+
+writeFile("linkObj.json", objString, 'utf-8', err => {
+    if(err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+
+    console.log('New JSON file has been saved.')
+})
